047e62aeeda363c607a1ad5799ada28e608ea009
malcolmkmd <hi@malcolmk.com>
2026-07-19 14:09:10 +0200

    Initial Commit

---
+++ StackOverflowSearch/StackOverflowSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackOverflowSearchApp.swift
+//  StackOverflowSearch
+//
+//  Created by Malcolm Collin on 2026/07/19.
+//
+
+import SwiftUI
+
+@main
+struct StackOverflowSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StackOverflowSearchTests/StackOverflowSearchTests.swift
@@ -0,0 +1,19 @@
+//
+//  StackOverflowSearchTests.swift
+//  StackOverflowSearchTests
+//
+//  Created by Malcolm Collin on 2026/07/19.
+//
+
+import Testing
+@testable import StackOverflowSearch
+
+struct StackOverflowSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
